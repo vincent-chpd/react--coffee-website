36fac62938630a49e213753697fe966b2eb345eb
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-05-07 13:04:19 +0100

    added navbar and hero pages

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,22 @@
+import "../assets/stylesheets/Hero.css";
+import "./Buttons.css";
+
 const Hero = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="hero">
+      <div className="hero--text">
+        <h1>Taste Best Coffee</h1>
+        <h4>Sip joy, savour moments, love coffee </h4>
+        <p>
+          Welcome to **insert name** cafe, where passion meets perfection in
+          every cup. Explore our exquisite range of handpicked blends and
+          single-origin beans.
+        </p>
+        <div className="hero--btn">
+          <button className="primary-btn">Order Now</button>
+          <button className="secondary-btn">Explore</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,29 @@
 import "../assets/stylesheets/Navbar.css";
+import { MdLocationPin } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar text-primary">
       <img
         src="https://t4.ftcdn.net/jpg/05/14/51/79/360_F_514517927_dXLi1DauUmrCaE3AkElsVgJ1jaYZMcSA.jpg"
         alt="Logo"
-        id="logo"
+        id="nav--logo"
       />
-      <ul className="flex">
+
+      <ul className="nav--links">
         <li>Home</li>
         <li>Gift</li>
         <li>Order</li>
         <li>Pay</li>
         <li>Store</li>
       </ul>
+
+      <div className="nav--search">
+        <MdLocationPin />
+        <h3>Find Store</h3>
+        <input type="text" />
+        <button>Login</button>
+      </div>
     </nav>
   );
 };
